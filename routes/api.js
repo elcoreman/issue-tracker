e7cd12e66690fa0ff3af9e87876a937f8d51cb99
Glitch (fragrant-primrose) <none>
2019-12-25 14:14:39 +0000

    ☔️🏚 Checkpoint ./routes/api.js:9298160/6069

--- routes/api.js
+++ routes/api.js
@@ -46,50 +46,43 @@ module.exports = app => {
       }
     })
 
-    .put((req, res)=> {
+    .put((req, res) => {
       let project = req.params.project;
       let issue = req.body._id;
       delete req.body._id;
       let updates = req.body;
-      for (let ele in updates) 
-        if (!updates[ele]) 
-          delete updates[ele];
-      if (updates.open) 
-        updates.open = String(updates.open) == "true";
+      for (let ele in updates) if (!updates[ele]) delete updates[ele];
+      if (updates.open) updates.open = String(updates.open) == "true";
       if (Object.keys(updates).length === 0) {
         res.send("no updated field sent");
       } else {
         updates.updated_on = new Date();
-        MongoClient.connect(CONNECTION_STRING, (err, db)=> {
+        MongoClient.connect(CONNECTION_STRING, (err, db) => {
           let collection = db.db("test1").collection(project);
           collection.findAndModify(
             { _id: new ObjectId(issue) },
             [["_id", 1]],
             { $set: updates },
             { new: true },
-            (err, doc) =>{
+            (err, doc) => {
               !err
                 ? res.send("successfully updated")
                 : res.send("could not update " + issue + " " + err);
-              //console.log(doc.value);
             }
           );
         });
       }
     })
 
-    .delete(function(req, res) {
-      var project = req.params.project;
-      var issue = req.body._id;
+    .delete((req, res) => {
+      let project = req.params.project;
+      let issue = req.body._id;
       if (!issue) {
         res.send("_id error");
       } else {
-        MongoClient.connect(CONNECTION_STRING, function(err, db) {
-          var collection = db.db("test1").collection(project);
-          collection.findAndRemove({ _id: new ObjectId(issue) }, function(
-            err,
-            doc
-          ) {
+        MongoClient.connect(CONNECTION_STRING, (err, db) => {
+          let collection = db.db("test1").collection(project);
+          collection.findAndRemove({ _id: new ObjectId(issue) }, (err, doc) => {
             !err
               ? res.send("deleted " + issue)
               : res.send("could not delete " + issue + " " + err);
