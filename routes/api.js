e92e353d2053916bb90c38740237b28d9a55367c
Glitch (fragrant-primrose) <none>
2019-12-25 11:56:40 +0000

    🌗🛍 Checkpoint ./routes/api.js:9298160/1707

--- routes/api.js
+++ routes/api.js
@@ -1,44 +1,32 @@
-/*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+"use strict";
 
-'use strict';
-
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-var ObjectId = require('mongodb').ObjectID;
-
-const CONNECTION_STRING = process.env.DB;
+var expect = require("chai").expect;
+var MongoClient = require("mongodb");
+var ObjectId = require("mongodb").ObjectID;
 
 
-module.exports = function (app) {
-MongoClient.connect(CONNECTION_STRING, function(err, db) {
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .post(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .put(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      var project = req.params.project;
-      
-    });
-  
+module.exports = function(app) {
+  MongoClient.connect(process.env.DB, function(err, client) {
+    const db = client.db("test");
+    app
+      .route("/api/issues/:project")
+
+      .get(function(req, res) {
+        var project = req.params.project;
+      })
+
+      .post(function(req, res) {
+        //var project = req.params.project;
+      db.issues.insert( { item: "card", qty: 15 } )
+
+      })
+
+      .put(function(req, res) {
+        var project = req.params.project;
+      })
+
+      .delete(function(req, res) {
+        var project = req.params.project;
+      });
   });
-    
 };
