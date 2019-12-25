dd88fa6bb85e86dbb208b9cb6e4dfefc7ae01b63
Glitch (fragrant-primrose) <none>
2019-12-25 12:07:41 +0000

    🍄🎈 Checkpoint ./routes/api.js:9298160/5083

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,6 @@ var expect = require("chai").expect;
 var MongoClient = require("mongodb");
 var ObjectId = require("mongodb").ObjectID;
 
-
 module.exports = function(app) {
   MongoClient.connect(process.env.DB, function(err, client) {
     const db = client.db("test");
@@ -16,9 +15,14 @@ module.exports = function(app) {
       })
 
       .post(function(req, res) {
-        //var project = req.params.project;
-      db.issues.insert( { item: "card", qty: 15 } )
-
+        var project = req.params.project;
+        db.issues.insert({
+          issue_title: req.body.issue_title,
+          issue_text: req.body.issue_text,
+          created_by: req.body.created_by,
+          assigned_to: req.body.assigned_to,
+          status_text: req.body.status_text
+        });
       })
 
       .put(function(req, res) {
