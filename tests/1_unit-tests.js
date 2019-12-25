b47b953eda7058803346998bf89f54bd6ef3f1e1
Glitch (fragrant-primrose) <none>
2019-12-25 15:00:36 +0000

    🍘🏐 Checkpoint ./tests/2_functional-tests.js:9298160/5048 ./tests/1_unit-tests.js:9298160/322

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,15 +1,3 @@
-/*
-*
-*
-*       FILL IN EACH UNIT TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]----
-*       (if additional are added, keep them at the very end!)
-*/
+var chai = require("chai");
 
-var chai = require('chai');
-
-suite('Unit Tests', function(){
-
-//none required
-
-});
+suite("Unit Tests", function() {});
